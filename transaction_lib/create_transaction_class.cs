68dcb89f6b38ca47098ad787d2d069524d28129f
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-04-15 19:37:49 -0400

    created main transaction added class

---
+++ transaction_lib/create_transaction_class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pallas.transaction
+{
+    public class createTransaction
+    {
+        public bool create()
+        {
+            bool isTransactionCompleted = false;
+            return isTransactionCompleted;
+        }
+    }
+}
